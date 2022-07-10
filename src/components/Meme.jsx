6dbc217f6a72342e622362387ea76169fa0149fd
ol3gatron <olegatron@gmail.com>
2022-07-10 14:24:39 +0300

    refactoring

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,12 +1,30 @@
+import React from "react"
 import memesData from "../data"
+import {useState} from "react"
 
 function Meme() {
+  const [memes, setMemes] = useState(memesData)
+
   function getMemeImage() {
-    const memesArray = memesData.data.memes
+    const memesArray = memes.data.memes
     const randomArrayNumber = Math.floor(Math.random() * memesArray.length)
     const url = memesArray[randomArrayNumber].url
+    return url
+  }
 
-    console.log(url)
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    imageUrl: "https://i.imgflip.com/1o00in.jpg",
+  })
+
+  function handleClick() {
+    setMeme(prevState => {
+      return {
+        ...prevState,
+        imageUrl: getMemeImage(),
+      }
+    })
   }
 
   return (
@@ -16,8 +34,9 @@ function Meme() {
           <input type="text" className="first-line" placeholder="Top text" />
           <input type="text" className="bottom-line" placeholder="Bottom text"/>
         </div>
-        <button onClick={getMemeImage}>Get a new meme image🖼️</button>
+        <button onClick={handleClick}>Get a new meme image🖼️</button>
       </div>
+      <img src={meme.imageUrl} alt="" />
     </div>
   )
 }
